f2657feebffd72883111f723d63aa0b7a6ea301b
Maciej Skrzypinski <tomek@ubuntu.(none)>
2013-04-27 21:05:23 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AZSGroup::Application.config.secret_token = 'd88f7963d4334fbdf7a666f1493a4432cadeed2b73b1e531e390e3a929a3fe8fc3e25e5a292181252ef1f69f40acf4d002528923b119be847b7d868f22ebd0bc'
